0442b6e3a509f19ad0467800c90e61a437869929
Ihor Venchak <iwenchak@gmail.com>
2020-08-27 15:07:47 +0300

    Second lesson

--- users-list/src/app/app.component.ts
+++ users-list/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'users-list';
+  title = 'Список користувачів. ';
   typesOfShoes: string[] = ['Ivan','Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
 }
--- users-list/src/app/app.module.ts
+++ users-list/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatListModule} from '@angular/material/list'; 
+import {MatListModule} from '@angular/material/list';
+import { UsersListComponent } from './users-list/users-list.component'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersListComponent
   ],
   imports: [
     BrowserModule,
